148eae1d554670b6e5ae26d302d9606e622ca154
Ahmad-MHK <117974936+Ahmad-MHK@users.noreply.github.com>
2023-05-31 12:42:23 +0200

    working on main
    main

---
+++ components/main.php
@@ -0,0 +1,40 @@
+<?php
+require_once('connection.php');
+?>
+
+<main>
+    <div class="img-main">
+    <img src="./assets/Main-img.jpg" alt="main-img" class="main.img">
+    </div>
+
+    <script src="https://kit.fontawesome.com/d97b87339f.js" crossorigin="anonymous"></script>
+    <div class="search-box">
+        <input class="search-input" type="text" placeholder="Search something..">
+        <button class="search-btn"><i class="fas fa-search"></i></button>
+    </div>
+
+    <div class="mid">
+    <div class="top-6-fav"><p>Top 6 favoriete ontdekkingen</p></div>
+    </div>
+
+
+            <div class="top-6-hotel">
+                <div class="hotel-block">
+                    <div class="hotel-block-left">
+                        <div class="hotel-block-img"> <img src="/assets/hotel review.jpeg" alt="hotel-review">  </div>
+                    </div>
+                    <div class="hotel-block-right">
+                        <div class="Hotel-Stars">Stars</div>
+                        <div class="gap-15px"></div>
+                        <div class="Hotel-naam">name</div>
+                        <div class="gap-15px"></div>
+                        <div class="Hotel-plaats">plaats</div>
+                        <div class="gap-15px"></div>
+                        <div class="Hotel-omschrijfing">asklghfa aghkabsg omschrijfing ashkbadls</div>
+                        <div class="gap-15px"></div>
+                        <div class="Hotel-prijs">9.99 euro</div>
+                    </div>
+                </div>
+            </div>
+
+</main>
--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 include("./components/head.php");
 include("./components/header.php") 
 ?>
-
+<?php
+include("./components/main.php") 
+?>
 <?php
 include("./components/footer.php") 
 ?>
