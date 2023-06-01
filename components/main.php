1c2bc8b30a7ee0a97b4eea43849a58be99d00769
Ahmad-MHK <117974936+Ahmad-MHK@users.noreply.github.com>
2023-06-01 12:38:39 +0200

    push
    push

--- components/main.php
+++ components/main.php
@@ -13,7 +13,13 @@ require_once('connection.php');
         <button class="search-btn"><i class="fas fa-search"></i></button>
     </div>
 
-    <div class="mid">
+    <div class="deals">
+        <div class="zomer-vakatine"></div>
+        <div class="last-mint"></div>
+        <div class="best-seller"></div>
+    </div>
+
+    <div class="mid-top-6-fav">
     <div class="top-6-fav"><p>Top 6 favoriete ontdekkingen</p></div>
     </div>
 
@@ -21,10 +27,16 @@ require_once('connection.php');
             <div class="top-6-hotel">
                 <div class="hotel-block">
                     <div class="hotel-block-left">
-                        <div class="hotel-block-img"> <img src="/assets/hotel review.jpeg" alt="hotel-review">  </div>
+                        <div class="hotel-block-img"> <img src="./assets/hotel review.jpeg" alt="hotel-review">  </div>
                     </div>
                     <div class="hotel-block-right">
-                        <div class="Hotel-Stars">Stars</div>
+                        <div class="Hotel-Stars">
+                            <img src="./assets/star.png" alt="star-1" class="star-1">
+                            <img src="./assets/U_star.png" alt="Star-2" class="star-1">
+                            <img src="./assets/U_star.png" alt="Star-3" class="star-1">
+                            <img src="./assets/U_star.png" alt="Star-4" class="star-1">
+                            <img src="./assets/U_star.png" alt="Star-5" class="star-1">
+                        </div>
                         <div class="gap-15px"></div>
                         <div class="Hotel-naam">name</div>
                         <div class="gap-15px"></div>
