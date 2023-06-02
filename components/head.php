1c4214ad903a7e181eccb7b340ca99e24ce03b0f
tomvr2003 <104300998+tomvr2003@users.noreply.github.com>
2023-06-02 13:32:35 +0200

    Index pagina af + databse connectie
    Index pagina af + databse connectie

--- components/head.php
+++ components/head.php
@@ -6,6 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="./style.css">
+  <script src="https://kit.fontawesome.com/6edbecffa8.js" crossorigin="anonymous"></script>    <link rel="stylesheet" href="style.css">
 </head>
 <body>
   
--- login.php
+++ login.php
@@ -1,27 +1,4 @@
 <?php session_start();
-    require("./components/connection.php");
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $usernameOrEmail = $_POST['username'];
-    $password = $_POST['password'];
-
-    $sql = "SELECT * FROM login WHERE (username = :username OR email = :email) AND password = :password";
-    $statement = $conn->prepare($sql);
-    $statement->bindParam(":username", $usernameOrEmail);
-    $statement->bindParam(":email", $usernameOrEmail);
-    $statement->bindParam(":password", $password);
-    $statement->execute();
-
-    $result = $statement->fetch(PDO::FETCH_ASSOC);
-
-    if ($result) {
-        $_SESSION["username"] = $result["username"];
-        header("Location: ./index.php");
-    } else {
-        echo "<p style='margin-bottom: 20px; font-weight: 500;'>Invalid username/email or password</p>";
-    }
-    }
-
     include("./components/head.php");
     include("./components/header.php");
 ?>
@@ -32,6 +9,30 @@
             <img src="./assets/banner-login.png" alt="banner-login" class="login-banner">
         </div>
         <div class="section-login-right">
+            <?php 
+                    require("./components/connection.php");
+
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                    $usernameOrEmail = $_POST['username'];
+                    $password = $_POST['password'];
+
+                    $sql = "SELECT * FROM login WHERE (username = :username OR email = :email) AND password = :password";
+                    $statement = $conn->prepare($sql);
+                    $statement->bindParam(":username", $usernameOrEmail);
+                    $statement->bindParam(":email", $usernameOrEmail);
+                    $statement->bindParam(":password", $password);
+                    $statement->execute();
+
+                    $result = $statement->fetch(PDO::FETCH_ASSOC);
+
+                    if ($result) {
+                        $_SESSION["username"] = $result["username"];
+                        header("Location: ./index.php");
+                    } else {
+                        echo "<p style='margin-bottom: 35px; font-weight: 500;'>Invalid username/email or password</p>";
+                    }
+                }
+            ?>
             <form action="login.php" method="POST" class="login-form">
                 <h1 style="margin-bottom: 10px;" class="login-form-title">Login</h1>
                 <input type="text" name="username" placeholder="Gebruikersnaam of Email">
@@ -39,6 +40,7 @@
                 <button style="background-color: #7189FF; margin-bottom: 20px;" type="submit" name="login">Log in</button>
             </form>
             <p>Geen account? <a href="./register.php"><span class="register-text">Registreer</span></a></p>
+            <p style="margin-top: 10px;">Wachtwoord vergeten? <a href="#"><span class="register-text">Reset</span></a></p>
         </div>
     </section>
 </main>
