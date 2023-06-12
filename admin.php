1978d579143757e55174fc6814b5e550fed6cbd7
Ahmad-MHK <117974936+Ahmad-MHK@users.noreply.github.com>
2023-06-12 14:34:23 +0200

    admin panel
    je krijg nu all informatie maar moet nog de edit en de delete nog maken

--- admin.php
+++ admin.php
@@ -1,55 +1,60 @@
-<?php session_start();
-    require_once('./components/connection.php');
-    include("./components/head.php");
-    include("./components/header.php");
+<?php
+session_start();
+require_once('./components/connection.php');
+include("./components/head.php");
+include("./components/header.php");
+
+// Retrieve data from the "reisbureau" table
+$sql = "SELECT * FROM reisbureau";
+$statement = $conn->query($sql);
+$reisbureauData = $statement->fetchAll(PDO::FETCH_ASSOC);
 ?>
 
 <main>
     <div id="admin-full-page">
         <div class="add-riesbru">
-            <a href="./add-riesbureau.php"><button class="add-ries">Add ries</button></a>
+            <a href="./add-riesbureau.php"><button class="add-ries">Add Ries</button></a>
             <a href="booking.php"><button class="add-ries">Booking</button></a>
-            <a href="Messages.php"><button class="add-ries">messages</button></a>
+            <a href="Messages.php"><button class="add-ries">Messages</button></a>
         </div>
         <table class="rwd-table">
-    <tbody>
-      <tr>
-        <th>Image</th>
-        <th>ID</th>
-        <th>Star</th>
-        <th>Omschrijving</th>
-        <th>Location</th>
-        <th>Prijs</th>
-        <th>Action</th>
-      </tr>
-      <tr>
-        <td data-th="Image">
-          UPS5005
-        </td>
-        <td data-th="ID">
-          001
-        </td>
-        <td data-th="Star">
-          4
-        </td>
-        <td data-th="Omschrijving">
-          deze hotel is de best en de goedst prijs voor jouw ja jouw broke ik weet je hebt geen geld
-        </td>
-        <td data-th="Location">
-          china
-        </td>
-        <td data-th="Prijs">
-          19,99
-        </td>
-        <td data-th="Action">
-            <a href="add-delete.php"><button class="delet-ries">delete ries</button></a>
-            <a href="add-edit.php"><button class="edit-ries">edit ries</button></a>
-        </td>
-      </tr>
-      </tr>
-    </tbody>
-  </table>
+            <tbody>
+                <tr>
+                    <th>Image</th>
+                    <th>ID</th>
+                    <th>Star</th>
+                    <th>Omschrijving</th>
+                    <th>Location</th>
+                    <th>Prijs</th>
+                    <th>Action</th>
+                </tr>
+                <?php foreach ($reisbureauData as $row) { ?>
+                    <tr>
+                        <td data-th="Image">
+                            <?php echo $row['image']; ?>
+                        </td>
+                        <td data-th="ID">
+                            <?php echo $row['id']; ?>
+                        </td>
+                        <td data-th="Star">
+                            <?php echo $row['star']; ?>
+                        </td>
+                        <td data-th="Omschrijving">
+                            <?php echo $row['omschrijving']; ?>
+                        </td>
+                        <td data-th="Location">
+                            <?php echo $row['location']; ?>
+                        </td>
+                        <td data-th="Prijs">
+                            <?php echo $row['prijs']; ?>
+                        </td>
+                        <td data-th="Action">
+                            <a href="add-delete.php"><button class="delet-ries">Delete Ries</button></a>
+                            <a href="add-edit.php"><button class="edit-ries">Edit Ries</button></a>
+                        </td>
+                    </tr>
+                <?php } ?>
+            </tbody>
+        </table>
     </div>
-
 </main>
-
