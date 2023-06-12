4d563c18ee99050e3d22cec833325c574c3b3628
Ahmad-MHK <117974936+Ahmad-MHK@users.noreply.github.com>
2023-06-12 13:55:36 +0200

    messages finish
    finish php now you can get everything from database and you can read them its finish

--- messages.php
+++ messages.php
@@ -1,9 +1,9 @@
-<?php session_start();
+<?php
+    session_start();
     require_once('./components/connection.php');
     include("./components/head.php");
     include("./components/header.php");
 ?>
-
 
 <main>
     <div id="admin-full-page">
@@ -13,34 +13,31 @@
             <a href="Messages.php"><button class="add-ries">messages</button></a>
         </div>
         <table class="rwd-table">
-    <tbody>
-      <tr>
-        <th>Name</th>
-        <th>Last name</th>
-        <th>Email</th>
-        <th>Phone</th>
-        <th>contacts</th>
-      </tr>
-      <tr>
-        <td data-th="name">
-          AHMAD
-        </td>
-        <td data-th="Last-name">
-          MAHOUK
-        </td>
-        <td data-th="Email">
-          bart@gmail.com
-        </td>
-        <td data-th="Phone">
-          06510925
-        </td>
-        <td data-th="contacts">
-          dat is echt een sexy website
-        </td>
-      </tr>
-      </tr>
-    </tbody>
-  </table>
+            <tbody>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>Message</th>
+                </tr>
+                <?php
+                $query = "SELECT * FROM contact";
+                $stmt = $conn->prepare($query);
+                $stmt->execute();
+                while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                    ?>
+                    <tr>
+                        <td data-th="First Name"><?php echo $row['first_name']; ?></td>
+                        <td data-th="Last Name"><?php echo $row['last_name']; ?></td>
+                        <td data-th="Email"><?php echo $row['email']; ?></td>
+                        <td data-th="Phone"><?php echo $row['phone']; ?></td>
+                        <td data-th="Message"><?php echo $row['message']; ?></td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
     </div>
-
 </main>
